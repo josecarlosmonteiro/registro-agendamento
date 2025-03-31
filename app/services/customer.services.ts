6f4a76db7b4cbb36fd29e7e0c4453d0841d1d9a1
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2025-03-31 11:38:47 -0300

    feat: implementar service de clientes (listar + criar).

---
+++ app/services/customer.services.ts
@@ -0,0 +1,22 @@
+import { TCreateCustomerService, TListCustomersService } from "../types/customers/customer.contracts";
+
+const listAllCustomers: TListCustomersService = async () => {
+  return [
+    { id: 'cliente1', name: 'cliente 1' },
+    { id: 'cliente2', name: 'cliente 2' },
+  ]
+}
+
+const createCustomer: TCreateCustomerService = async (params) => {
+  console.log(params);
+
+  return {
+    ...params.customer,
+    id: 'id-criado-agora',
+  };
+}
+
+export const customersServices = {
+  findAll: listAllCustomers,
+  create: createCustomer,
+}
